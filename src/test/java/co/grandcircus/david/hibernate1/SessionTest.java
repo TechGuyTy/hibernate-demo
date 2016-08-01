85b6f021407320840d3788dada77ab50a5b840b1
David Wolverton <dwolvert@gmail.com>
2016-07-31 21:45:41 -0400

    session attachment example

--- src/test/java/co/grandcircus/david/hibernate1/SessionTest.java
+++ src/test/java/co/grandcircus/david/hibernate1/SessionTest.java
@@ -6,12 +6,15 @@ import org.hibernate.Transaction;
 import org.junit.Test;
 
 import co.grandcircus.david.hibernate1.config.HibernateConfigurer;
+import co.grandcircus.david.hibernate1.dao.DaoRegistry;
+import co.grandcircus.david.hibernate1.dao.PersonDao;
 import co.grandcircus.david.hibernate1.model.Course;
 import co.grandcircus.david.hibernate1.model.Person;
 
 public class SessionTest {
 	
 	private SessionFactory sessionFactory = HibernateConfigurer.getSessionFactory();
+	private PersonDao personDao = DaoRegistry.getPersonDao();
 
 	@Test
 	public void testRollbackTransaction() {
@@ -26,11 +29,45 @@ public class SessionTest {
 		person.setName("Anonymous");
 		person.setInstructor(true);
 		person.setCourseId(newCourse.getId());
-		session.save(newCourse);
+		session.save(person);
 		
 		session.flush();
 		
+		System.out.println(session.get(Person.class, person.getId()));
+		System.out.println(session.get(Course.class, newCourse.getId()));
+		
+		System.out.println("Rollback.");
 		transaction.rollback();
+		
+		System.out.println(session.get(Person.class, person.getId()));
+		System.out.println(session.get(Course.class, newCourse.getId()));
+		
 		session.close();
+	}
+	
+	@Test
+	public void sessionAttachment() {
+		Session session = sessionFactory.openSession();
+		Transaction transaction = session.beginTransaction();
+		
+		Person person = new Person();
+		person.setName("Anonymous");
+		person.setInstructor(false);
+		person.setCourseId(1);
+		session.save(person);
+		System.out.println(session.get(Person.class, person.getId()));
+		
+		person.setName("New Name");
+		
+		transaction.commit();
+		
+		System.out.println(session.get(Person.class, person.getId()));
+		session.close();
+		
+		person.setName("Yet another name.");
+		
+		System.out.println(personDao.getById(person.getId()));
+		
+		System.out.println(person);
 	}
 }
